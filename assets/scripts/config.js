1ad3d3d81faa61ee7ce3a6bbe97390a251f71499
Bowers96 <bowersc96@gmail.com>
2018-04-23 23:56:23 -0700

    initialize repo

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = {
+  apiOrigins: {
+    production: 'https://secret-journey-84193.herokuapp.com',
+    development: 'http://localhost:4741'
+  },
+};
+
+module.exports = config;
---
+++ assets/scripts/store.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const store = {
+  host: 'http://localhost:4741/',
+};
+
+window.store = store;
+
+module.exports = store;
